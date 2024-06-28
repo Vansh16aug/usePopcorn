592360a63f1645ac863f636aac6fd79555a9403a
Vansh16aug <kumarvansh16aug@gmail.com>
2024-06-28 23:05:04 +0530

    used PropTypess

--- src/index.js
+++ src/index.js
@@ -10,5 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
